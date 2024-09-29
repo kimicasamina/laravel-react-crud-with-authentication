8d6f3c89e13f5b34d711281ea924eb88b5a78e0a
kimi.casamina <kimi.casamina@gmail.com>
2024-09-29 15:53:48 +0800

    added state context

--- laravel-react-crud-and-authentication/src/components/DefaultLayout.jsx
+++ laravel-react-crud-and-authentication/src/components/DefaultLayout.jsx
@@ -1,7 +1,13 @@
 import React from "react";
-import { Outlet } from "react-router-dom";
+import { Outlet, Navigate } from "react-router-dom";
+import { useStateContext } from "../context/contextProvider";
 
 export default function DefaultLayout() {
+    const { user, token } = useStateContext();
+
+    if (!token) {
+        return <Navigate to="/login" />;
+    }
     return (
         <div className="w-full ">
             <div className="">Default</div>
